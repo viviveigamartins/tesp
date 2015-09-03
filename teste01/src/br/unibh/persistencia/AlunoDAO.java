b150ac760e96f6c6235d6d8c4b31a8a73200613c
11320835 <11320835@ESTB80715.unibh.edu>
2015-09-02 21:08:55 -0300

    r

--- teste01/src/br/unibh/persistencia/AlunoDAO.java
+++ teste01/src/br/unibh/persistencia/AlunoDAO.java
@@ -3,6 +3,7 @@ package br.unibh.persistencia;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,38 +11,55 @@ import br.unibh.teste01.entidades.Aluno;
 
 public class AlunoDAO implements DAO<Aluno, Long> {
 	private static Connection con = null;
+	private static SimpleDateFormat  df = new SimpleDateFormat ("yyyy -mm-dd");
 
 	@Override
 	public Aluno find(Long id) {
-		try{
-			PreparedStatement p = JDBCUtil.getConnection().
-					prepareStatement("select * from tb_aluno where id =?");
-			
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where id =?");
+
 			p.setLong(1, id);
 			ResultSet res = p.executeQuery();
-			if(res.next()) {
-				return new Aluno(
-									res.getLong("id"),
-									res.getString("noem"),
-									res.getString("cpf"),
-									res.getString("matricula"),
-									res.getDate("dataAniversario"));
+			if (res.next()) {
+				return new Aluno(res.getLong("id"), res.getString("noem"), res.getString("cpf"),
+						res.getString("matricula"), res.getDate("dataAniversario"));
 			}
-		}catch (Exception e) {
-					// TODO: handle exception
-					e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
 
-				} finally {
+		} finally {
 
-					JDBCUtil.closeConnection();
+			JDBCUtil.closeConnection();
 
-				}
+		}
 
-				return null;
+		return null;
 	}
+
 	@Override
 	public void insert(Aluno t) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("insert into tb_aluno"
+					+ " (nome, cpf, matricula, dataAnviersario"
+					+ " values (?, ?, ?, ?)");
+
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setString(3, t.getNome());
+			p.setString(4, t.getMatricula());
+			p.setString(5, df.format(t.getDataAniversario()));
+			p.executeUpdate();
+			
+			} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
 
 	}
 
