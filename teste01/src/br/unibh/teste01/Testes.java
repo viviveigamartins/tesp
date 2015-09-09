1ca5c07980cd2f5331a495e7709d15b7a3192bd4
11320835 <11320835@ESTB80715.unibh.edu>
2015-09-09 20:56:08 -0300

    f

---
+++ teste01/src/br/unibh/teste01/Testes.java
@@ -0,0 +1,60 @@
+package br.unibh.teste01;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.unibh.persistencia.AlunoDAO;
+import br.unibh.teste01.entidades.Aluno;
+
+public class Testes {
+
+	@Test
+	public void testeFindAll() {
+
+		AlunoDAO dao = new AlunoDAO();
+		List<Aluno> lista = dao.findAll();
+		Assert.assertEquals(lista.size(), 101);
+
+	}
+
+	@Test
+	public void testeAlunoFind() {
+
+		AlunoDAO dao = new AlunoDAO();
+		Aluno a = dao.find(95L);
+		Assert.assertEquals(a.getNome(), "Vanna B. Kane");
+
+	}
+	
+	@Test
+	public void AlunoInsertEDelete(){
+		
+		AlunoDAO dao = new AlunoDAO();
+		Aluno a = new Aluno(null, "Aline","12548976358","54896758", new Date()); 
+		dao.insert(a);
+		Aluno b = dao.find("Aline");
+		Assert.assertNotNull(b);
+		dao.delete(b);
+		Aluno c = dao.find("Aline");
+		Assert.assertNull(c);
+	}
+	@Test
+	public void AlunoUpdate(){
+		
+		AlunoDAO dao = new AlunoDAO();
+		Aluno a = new Aluno(null, "Beltrano","22598916358","54896758", new Date()); 
+		dao.insert(a);
+		Aluno b = dao.find("Beltrano");
+		b.setNome("Ciclano");
+		dao.update(b);
+		Aluno c = dao.find("Ciclano");
+		Assert.assertNotNull(c);
+		dao.delete(c);
+	}
+	
+	
+
+}
