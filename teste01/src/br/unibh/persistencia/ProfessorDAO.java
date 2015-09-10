c7da45e91b71c4cfa062c5b6aa86b546bfc2c1c8
11320835 <11320835@ESTB80715.unibh.edu>
2015-09-09 21:30:19 -0300

    final

--- teste01/src/br/unibh/persistencia/ProfessorDAO.java
+++ teste01/src/br/unibh/persistencia/ProfessorDAO.java
@@ -1,40 +1,165 @@
 package br.unibh.persistencia;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import br.unibh.teste01.entidades.Aluno;
 import br.unibh.teste01.entidades.Professor;
 
 public class ProfessorDAO implements DAO<Professor,Long> {
 
 	@Override
 	public Professor find(Long id) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_professor where id =?");
+
+			p.setLong(1, id);
+			ResultSet res = p.executeQuery();
+			if (res.next()) {
+				return new Professor(
+						res.getLong("id"),
+						res.getString("nome"),
+						res.getString("cpf"),
+						res.getBigDecimal("salario")); 
+						
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
 		return null;
 	}
+	public Professor find(String nome) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().
+					prepareStatement("select * from tb_professor where nome like ?");
+
+			p.setString(1, nome + "%");
+			ResultSet res = p.executeQuery();
+			if (res.next()) {
+				return new Professor(
+						res.getLong("id"),
+						res.getString("nome"),
+						res.getString("cpf"),
+						res.getBigDecimal("salario")); 
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
+		return null;
+	}
+	
+
 
 	@Override
 	public void insert(Professor t) {
-		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("insert into tb_professor"
+					+ " (nome, cpf, salario)"
+					+ " values (?, ?, ?)");
+
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setBigDecimal(3, t.getSalario());
+			p.executeUpdate();
+			
+			} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
 	}
 
 	@Override
 	public void update(Professor t) {
-		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
+					"update tb_professor set nome = ?, cpf = ?,"
+					+ " salario = ?"
+					+ " where id = ?");
+
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setBigDecimal(3, t.getSalario());
+			p.setLong(4,  t.getId());
+			p.executeUpdate();
+			
+			} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
 	}
 
 	@Override
 	public void delete(Professor t) {
-		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("delete from tb_professor where id = ?");
+			p.setLong(1, t.getId());
+			p.executeUpdate();
+			
+			} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
 	}
 
 	@Override
 	public List<Professor> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Professor> lista = new ArrayList<Professor>();
+
+		try {
+
+			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_professor").executeQuery();
+
+			while (res.next()) {
+				lista.add(new Professor(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getBigDecimal("salario")));
+			
+
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		} finally {
+
+			JDBCUtil.closeConnection();
+
+		}
+
+		return lista;
 	}
-	
 
 }
--- teste01/src/br/unibh/teste01/Testes.java
+++ teste01/src/br/unibh/teste01/Testes.java
@@ -1,5 +1,6 @@
 package br.unibh.teste01;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -7,7 +8,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import br.unibh.persistencia.AlunoDAO;
+import br.unibh.persistencia.ProfessorDAO;
 import br.unibh.teste01.entidades.Aluno;
+import br.unibh.teste01.entidades.Professor;
 
 public class Testes {
 
@@ -51,6 +54,52 @@ public class Testes {
 		b.setNome("Ciclano");
 		dao.update(b);
 		Aluno c = dao.find("Ciclano");
+		Assert.assertNotNull(c);
+		dao.delete(c);
+	}
+	
+	//classe Professor
+	
+	@Test
+	public void testeProfessorFindAll() {
+
+		ProfessorDAO dao = new ProfessorDAO();
+		List<Professor> lista = dao.findAll();
+		Assert.assertEquals(lista.size(), 100);
+
+	}
+
+	@Test
+	public void testeProfessorFind() {
+
+		ProfessorDAO dao = new ProfessorDAO();
+		Professor a = dao.find(25L);
+		Assert.assertEquals(a.getNome(), "Sonia W. Schneider");
+
+	}
+	
+	@Test
+	public void ProfessorInsertEDelete(){
+		
+		ProfessorDAO dao = new ProfessorDAO();
+		Professor a = new Professor(null, "Pedro","12548976359", new BigDecimal(1254858)); 
+		dao.insert(a);
+		Professor b = dao.find("Pedro");
+		Assert.assertNotNull(b);
+		dao.delete(b);
+		Professor c = dao.find("Paulo");
+		Assert.assertNull(c);
+	}
+	@Test
+	public void ProfessorUpdate(){
+		
+		ProfessorDAO dao = new ProfessorDAO();
+		Professor a = new Professor(null, "Beltrano","22598916358", new BigDecimal(2658974)); 
+		dao.insert(a);
+		Professor b = dao.find("Beltrano");
+		b.setNome("Ciclano");
+		dao.update(b);
+		Professor c = dao.find("Ciclano");
 		Assert.assertNotNull(c);
 		dao.delete(c);
 	}
